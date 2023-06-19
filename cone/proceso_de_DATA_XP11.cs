1141ff7cb2f05536c16a3198a24ef3683cb16368
Fabián Osorio <srholazul@gmail.com>
2023-06-19 18:01:57 -0400

    Update proceso_de_DATA_XP11.cs

--- cone/proceso_de_DATA_XP11.cs
+++ cone/proceso_de_DATA_XP11.cs
@@ -1,13 +1,20 @@
 using System;
 public class Demo {
    public static void Main() {
+    byte[] array={68,65,84,65,42,20,0,0,0,182,218,61,66,146,157,244,194,241,251,201,67,123,13,91,190,0,0,128,63,240,142,206,67,0,0,62,66,0,0,244,194}; 
       byte[] arr = {182,218,61,66,146,157,244,194,241,251,201,67,123,13,91,190,0,0,128,63,240,142,206,67,0,0,62,66,0,0,244,194};
-      Console.WriteLine("Byte Array = {0} ", BitConverter.ToString(arr));
-      for (int i = 0; i < arr.Length - 4; i = i + 4) {
-         float res = BitConverter.ToSingle(arr, i);
-         Console.WriteLine("Value = "+arr[i]);
+      string segmento=BitConverter.ToString(array) ;
+      Console.WriteLine("Byte Array = {0} ", segmento);
+      Console.WriteLine("Value 1 = "+array[0]);
+      //for (int i = 0; i <= 8; i = i + 1) {
+      //   Console.WriteLine("Value 1 = "+array[i]);
+      //}
+      for (int i = 9; i < array.Length; i = i + 4) {
+         float res = BitConverter.ToSingle(array, i);
+         //Console.WriteLine("Value = "+arr[i]);
          Console.WriteLine("Result = "+res);
       }
    }
 }
-//68,65,84,65,42,20,0,0,0,182,218,61,66,146,157,244,194,241,251,201,67,123,13,91,190,0,0,128,63,240,142,206,67,0,0,62,66,0,0,244,194,
+//68,65,84,65,42, 20,0,0,0, 182,218,61,66, 146,157,244,194, 241,251,201,67, 123,13,91,190, 0,0,128,63, 240,142,206,67, 0,0,62,66, 0,0,244,194,
+//                              1               2                   3          4                 5           6             7           8          
