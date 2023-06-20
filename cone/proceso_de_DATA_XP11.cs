0880fdcbd9db9a31cb8f58c7a760d67e3252dfeb
Fabián Osorio <srholazul@gmail.com>
2023-06-19 20:50:47 -0400

    Update proceso_de_DATA_XP11.cs

--- cone/proceso_de_DATA_XP11.cs
+++ cone/proceso_de_DATA_XP11.cs
@@ -9,11 +9,13 @@ public class Demo {
       //for (int i = 0; i <= 8; i = i + 1) {
       //   Console.WriteLine("Value 1 = "+array[i]);
       //}
+      Console.Write("Result: [");
       for (int i = 9; i < array.Length; i = i + 4) {
          float res = BitConverter.ToSingle(array, i);
          //Console.WriteLine("Value = "+arr[i]);
-         Console.WriteLine("Result = "+res);
+         Console.Write("  "+res);
       }
+      Console.WriteLine("  ]");
    }
 }
 //68,65,84,65,42, 20,0,0,0, 182,218,61,66, 146,157,244,194, 241,251,201,67, 123,13,91,190, 0,0,128,63, 240,142,206,67, 0,0,62,66, 0,0,244,194,
